f3f92b3e95a4ed7d99f188baf50e59f748bd7fdc
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-04-05 11:49:45 +0200

    added get and post

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+const port = 3000;
+const host = "localhost";
+
+const cats = [
+  {
+    name: "Cocos",
+    age: 5,
+    color: "Cream",
+    id: 1,
+  },
+  {
+    name: "Twister",
+    age: 3,
+    color: "Black",
+    id: 2,
+  },
+];
+
+app.use(express.json());
+
+app.get("/api/cats", (req, res) => {
+  res.json(cats);
+});
+
+app.post("/api/cats", (req, res) => {
+  cats.push(req.body);
+  res.status(201);
+  res.send("Cat added");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
