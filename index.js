d07c89b30b3ae829b9643ad9ed45faf5cf9a79e9
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-04-06 18:27:19 +0200

    added put and delete and uuid

--- index.js
+++ index.js
@@ -1,45 +1,69 @@
 import express from "express";
+import { v4 as uuidV4 } from "uuid";
 const app = express();
 const port = 3000;
 const host = "localhost";
 
 let cats = [
-  {
-    name: "Cocos",
-    age: 2,
-    color: "Cream",
-    id: 1,
-  },
-  {
-    name: "Twister",
-    age: 5,
-    color: "Gray",
-    id: 2,
-  },
+  // {
+  //   name: "Cocos",
+  //   age: 2,
+  //   color: "Cream",
+  //   id: uuidV4(),
+  // },
+  // {
+  //   name: "Twister",
+  //   age: 5,
+  //   color: "Gray",
+  //   id: uuidV4(),
+  // },
 ];
 
 app.use(express.json());
 
 app.get("/api/cats", (req, res) => {
+  if (cats.length === 0) {
+    res.status(404);
+    res.send("No cats found");
+  }
   res.status(200);
   res.json(cats);
 });
 
 app.post("/api/cats", (req, res) => {
-  cats.push(req.body);
+  const cat = req.body;
+  cats.push({ ...cat, id: uuidV4() });
   res.status(201);
-  res.send("Cat added");
+  // res.send("Cat added");
+  res.json(cats);
 });
 
-app.delete("/api/cats:id", (req, res) => {
-  const id = req.params.id;
-  cats = cats.filter((i) => {
-    if (i.id !== id) {
-      return true;
+app.put("/api/cats/:id", (req, res) => {
+  const catId = req.params.id;
+  const { age, name, color } = req.body;
+
+  cats = cats.map(function (cat) {
+    if (cat.id === catId) {
+      return {
+        name,
+        age,
+        color,
+        id: cat.id,
+      };
     }
-    return false;
+    return cat;
   });
-  res.send("Cat has been removed");
+  res.status(200);
+  res.json(cats);
+});
+
+app.delete("/api/cats/:id", (req, res) => {
+  const catId = req.params.id;
+  cats = cats.filter(function (cat) {
+    return cat.id !== catId;
+  });
+  res.status(200);
+  res.json(cats);
 });
 
 app.listen(port, () => {
